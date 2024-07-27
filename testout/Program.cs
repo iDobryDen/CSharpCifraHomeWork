bd2cb1954a5b21d8dafcdcb9819fa8e124c204de
iDobryDen <59179395+iDobryDen@users.noreply.github.com>
2024-07-27 13:41:43 +0400

    testout and refout is understand

---
+++ testout/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace testout;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        
+        int sum2 = Sum(150, 150, out int sum);
+        Console.WriteLine(sum2);
+    }
+
+    static int Sum(int a, int b, out int sum)
+    {
+        return sum = a + b;
+    }
+}
+
---
+++ testref/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace testref;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        
+        int age = 0;
+        Increment(ref age);
+        Console.WriteLine(age);
+    }
+
+    static void Increment (ref int value)
+    {
+        
+        value++;
+        
+    }
+}
+
